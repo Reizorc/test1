79baeb7e15354bdfb779f6af35e06254371d3b35
lapinozz <monstrefou@gmail.com>
2014-04-21 11:51:54 -0400

    collision intelligente et refonte du code

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 
 #define NB_BLOCS_L 9
 #define NB_BLOCS_H 12
-
-
-void affiche(sf::RenderWindow& app, char* gamemap[NB_BLOCS_H][NB_BLOCS_L]);
 
     sf::Font font;
     sf::Sprite grass;
@@ -15,15 +14,20 @@ void affiche(sf::RenderWindow& app, char* gamemap[NB_BLOCS_H][NB_BLOCS_L]);
 
 struct Title
 {
-    sf::Vector2i x, y;
-    sf::Vector2i realx, realy;
+    sf::Vector2i pos;
+    sf::Vector2i realPos;
+    int cantWalk;
+    int ID;
 
 };
 
+void affiche(sf::RenderWindow& app, Title* gamemap[NB_BLOCS_H][NB_BLOCS_L]);
+sf::Vector2i getPersoTitle(sf::Sprite perso);
 
 int main()
 {
 
+    srand (time(NULL));
 
     sf::RenderWindow app(sf::VideoMode(NB_BLOCS_H*64, NB_BLOCS_L*64), "TILE MAP!!)");
 
@@ -35,21 +39,36 @@ int main()
 
 
 
-    char* gamemap[NB_BLOCS_H][NB_BLOCS_L] =
+    Title* gamemap[NB_BLOCS_H][NB_BLOCS_L];
+//    {
+//    {'0','0','0','0','0','0','0','0','0'},
+//    {'0','0','0','0','0','0','0','0','0'},
+//    {'0','0','0','0','0','0','0','0','0'},
+//    {'0','0','0','0','0','0','0','0','0'},
+//    {'0','0','0','1','0','0','0','0','0'},
+//    {'0','0','0','0','0','0','0','0','0'},
+//    {'0','0','0','0','0','0','0','0','0'},
+//    {'0','0','0','0','0','0','0','0','0'},
+//    {'0','0','0','0','0','0','0','0','0'},
+//    {'0','0','0','0','0','0','0','0','0'},
+//    {'0','0','0','0','0','0','0','0','0'},
+//    {'0','0','0','0','0','0','0','0','0'}
+//    };
+
+    for(int x = 0; x < NB_BLOCS_H; x++)
     {
-    {'0','0','0','0','0','0','0','0','0'},
-    {'0','0','0','0','0','0','0','0','0'},
-    {'0','0','0','0','0','0','0','0','0'},
-    {'0','0','0','0','0','0','0','0','0'},
-    {'0','0','0','1','0','0','0','0','0'},
-    {'0','0','0','0','0','0','0','0','0'},
-    {'0','0','0','0','0','0','0','0','0'},
-    {'0','0','0','0','0','0','0','0','0'},
-    {'0','0','0','0','0','0','0','0','0'},
-    {'0','0','0','0','0','0','0','0','0'},
-    {'0','0','0','0','0','0','0','0','0'},
-    {'0','0','0','0','0','0','0','0','0'}
-    };
+        for(int y = 0; y < NB_BLOCS_L; y++)
+        {
+            gamemap[x][y] = new Title;
+            Title* title = gamemap[x][y];
+            title->ID = rand() % 2 + 0; // rand() % max + min
+            title->pos = sf::Vector2i(x, y);
+            if(title->ID == 1)
+                title->cantWalk = 1;
+            else
+                title->cantWalk = 0;
+        }
+    }
 
 
     while (app.isOpen())
@@ -77,43 +96,29 @@ int main()
                         break;
 
                     case sf::Keyboard::Left :
-                        if(posX >= NB_BLOCS_H/64 +64){
-                            if(posY != posYstone){
+                        if(posX >= NB_BLOCS_H/64 +64)
+                        {
+                            if(!gamemap[getPersoTitle(perso).x-1][getPersoTitle(perso).y]->cantWalk)
                                 perso.move(-64,0);
-                            }
-                            else if(posX >= posXstone + 65 || posX < posXstone){
-                                perso.move(-64,0);
-                            }
                         }
                         break;
 
                     case sf::Keyboard::Right :
                         if(posX <= NB_BLOCS_L*64 +64)
-                            if(posY != posYstone){
+                            if(!gamemap[getPersoTitle(perso).x+1][getPersoTitle(perso).y]->cantWalk)
                                 perso.move(64,0);
-                            }
-                            else if(posX <= posXstone -65 || posX > posXstone){
-                                perso.move(64,0);
-                            }
                         break;
 
                     case sf::Keyboard::Up :
                         if(posY >= NB_BLOCS_H/64 +64)
-                            if(posX != posXstone){
+                            if(!gamemap[getPersoTitle(perso).x][getPersoTitle(perso).y-1]->cantWalk)
                                 perso.move(0,-64);
-                            }else if(posY >= posYstone +65 || posY < posYstone){
-                                perso.move(0,-64);
-                            }
                         break;
 
                     case sf::Keyboard::Down :
                         if(posY < NB_BLOCS_L*64 -64)
-                            if(posX != posXstone){
+                            if(!gamemap[getPersoTitle(perso).x][getPersoTitle(perso).y+1]->cantWalk)
                                 perso.move(0,64);
-                            }
-                            else if(posY <= posYstone -65 || posY > posYstone){
-                                perso.move(0,64);
-                            }
                         break;
 
 
@@ -142,7 +147,12 @@ int main()
     return EXIT_SUCCESS;
 }
 
-void affiche(sf::RenderWindow &app, char* gamemap[NB_BLOCS_H][NB_BLOCS_L])
+sf::Vector2i getPersoTitle(sf::Sprite sprite)
+{
+    return sf::Vector2i(sprite.getPosition().x/64, sprite.getPosition().y/64);
+}
+
+void affiche(sf::RenderWindow &app, Title* gamemap[NB_BLOCS_H][NB_BLOCS_L])
 {
 
     sf::Texture tex_grass;
@@ -158,12 +168,13 @@ void affiche(sf::RenderWindow &app, char* gamemap[NB_BLOCS_H][NB_BLOCS_L])
     {
         for( int y = 0; y < NB_BLOCS_L; y++)
         {
-            if(gamemap[x][y] == '0')
+            Title* title = gamemap[x][y];
+            if(title->ID == 0)
             {
                 grass.setPosition(64*(x+1)-64, 64*(y+1)-64);
                 app.draw(grass);
             }
-            if(gamemap[x][y] == '1')
+            if(title->ID == 1)
             {
                 stone.setPosition(64*(x+1)-64, 64*(y+1)-64);
                 app.draw(stone);
